8a6d828557c090945b015e2d450cf7399545672b
C22410612 <C22410612@mytudublin.ie>
2024-02-21 17:35:25 +0000

    Added camera movement to follow the player for game5

---
+++ VolaTileU/Assets/Scripts/Game5CameraController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game5CameraController : MonoBehaviour
+{
+    // Camera Variables
+
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z); // Moves the camera with the player
+    }
+}
