bf0d59ae25a8a878c220cbb1b52e79b12d6d4c69
C22410612 <C22410612@mytudublin.ie>
2024-03-11 17:03:46 +0000

    Added a delay between the calculation of the spawn position and the actual spawning - Done using a CoRoutine - Shifted spawning logic around methods

--- VolaTileU/Assets/Scripts/ScriptsGame4/Game4SpawnManager.cs
+++ VolaTileU/Assets/Scripts/ScriptsGame4/Game4SpawnManager.cs
@@ -9,12 +9,23 @@ public class Game4SpawnManager : MonoBehaviour
     
     
     // Spawner Variables
-    private List<Vector3> spawnPositions;
-    private Transform spawnLocation;
-    private float radius = 8.0f;
-    private float numberOfSpawnLocations = 12;
-    public static float spawnTimer;
-    public static float spawnDelay = 25.0f;
+
+        // Circle radius
+        private List<Vector3> spawnPositions;
+        private float radius = 8.0f;
+        private float numberOfSpawnLocations = 12;
+
+        // Instantiate
+        private int circlePosition;
+        Vector3 direction;
+        Quaternion rotation;
+
+        // Timers
+        public static float spawnTimer;
+        public static float spawnDelay = 1.0f;
+
+        public static float dodgeTimer;
+        public static float dodgeDelay = 0.5f; 
 
     // Player Variables
     public GameObject player;
@@ -23,53 +34,61 @@ public class Game4SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        spawnLocation = player.transform;
+        // Initial Variable Assignments
+        spawnTimer = spawnDelay;
+        dodgeTimer = dodgeDelay;
+
+        // Start the spawning logic
+        StartCoroutine(SpawnRoutine());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        CalculateSpawnRange();
 
-        SpawnLasers();
+    // Handles spawning and the delays between spawn and dodge timers
+    private IEnumerator SpawnRoutine()
+    {
+        // Continue spawning // TODO make conditional based on if the game is in view or not.
+        while(true)
+        {
+            CalculateSpawnRange();
+            yield return new WaitForSeconds(dodgeDelay);
+
+            SpawnLasers();
+            yield return new WaitForSeconds(spawnDelay);
+        }
     }
 
 
     private void SpawnLasers()
-    {
-        if(spawnTimer <= 0)
-        {
-            int circlePosition = Random.Range(0, spawnPositions.Count);
-
-            Vector3 direction = spawnPositions[circlePosition] - player.transform.position;
-            Quaternion rotation = Quaternion.LookRotation(Vector3.forward, direction);
-
-            Instantiate(laserPrefab, spawnPositions[circlePosition], rotation);
-
-            spawnTimer = spawnDelay;
-        }
-
-        else
-        {
-            spawnTimer -= Time.fixedDeltaTime;
-        }
+    {       
+        Instantiate(laserPrefab, spawnPositions[circlePosition], rotation);
     }
+
 
    // Creates a list of spawn positions around in a circle radius around the player that enemies can spawn from
     private void CalculateSpawnRange()
     {
-        float angleStep = 360f / numberOfSpawnLocations;;
+        float angleStep = 360.0f / numberOfSpawnLocations; // Number of possible locations in the circle to spawn from
         
         spawnPositions = new List<Vector3>(); // List to store calculated spawn positions
 
+        // For each possible location calculate where in the circle it should spawn
         for(int i = 0; i < numberOfSpawnLocations; i++)
         {
+            // Circle math
             float angle = angleStep * i;
             float x = Mathf.Sin(Mathf.Deg2Rad * angle) * radius;
             float y = Mathf.Cos(Mathf.Deg2Rad * angle) * radius;
-            Vector3 spawnPosition = player.transform.position + new Vector3(x, y, 0);
+
+            // Add the vector3 to the list
+            Vector3 spawnPosition = player.transform.position + new Vector3(x, y, 0); 
             spawnPositions.Add(spawnPosition);
-        }
+            
+            circlePosition = Random.Range(0, spawnPositions.Count); // Pick the possible location
+
+            // Rotate and point towards the player
+            direction = spawnPositions[circlePosition] - player.transform.position;
+            rotation = Quaternion.LookRotation(Vector3.forward, direction);
+        }    
     }
     
 }
